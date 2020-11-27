e5eb5f945194f1438f77bc65678c1038e6d91691
renzo <u201415624@upc.edu.pe>
2020-11-27 13:34:49 -0500

    graficarEcuaciones

--- extract.py
+++ extract.py
@@ -1,53 +1,86 @@
+import random as rnd
+import numpy as np
+
+def getRandomCurve(lenght,variance=50):
+	randi = rnd.randint(1,variance)/variance
+	a = list(range(lenght+1)) 
+	b = list(a)
+	for i in range(lenght+1):
+		b[i] = (randi*i)
+	return [a, b]
+
 def getXAndY(text):
-    text = text.replace(" ", "")
+	text = text.replace(" ", "")
+	idx = text.find("x")
+	idy = text.find("y")
+	x = int(text[0:idx])
+	y = int(text[idx+1:idy])
+	return x, y
 
-    idx = text.find("x")
-    idy = text.find("y")
+def stripIneq(text):
+	# Quitar los espacios
+	text = text.replace(" ", "")
+	# Obtener la posicion del <, > o =
+	idr = max(text.find("<"), text.find(">"), text.find("="))
+	x, y, r = 0, 0, 0
+	# Obtener x si esta presente
+	if (idx:=text.find("x")) >= 0:
+		start = max(text[:idx].rfind("y"),
+					text[:idx].rfind("<"),
+					text[:idx].rfind(">"),
+					text[:idx].rfind("="))
+		x = text[start+1:idx]
+		if x == "" or x == "+": x = 1
+		if x == "-": x = -1
+		if idx > idr: x = -1 * int(x)
+		else: x = int(x)
+	# Obtener y si esta presente
+	if (idy:=text.find("y")) >= 0:
+		start = max(text[:idy].rfind("x"),
+					text[:idy].rfind("<"),
+					text[:idy].rfind(">"),
+					text[:idy].rfind("="))
+		y = text[start+1:idy]
+		if y == "" or y == "+": y = 1
+		if y == "-": y = -1
+		if idy > idr: y = -1 * int(y)
+		else: y = int(y)
+	# Obtener la respuesta
+	if idr > idx and idr > idy:
+		r = int(text[idr+1:])
+	# Obtener la orientacion # 0 Menor"<" | 1 Mayor">"
+	#s = int(text.find(">")>0)
+	# Obtener si hay igual o no # 0 No | 1 Si
+	#e = max(0,int(text.find("=")>0))
+	return x, y, r #,s,e
 
-    x = int(text[0:idx])
-    y = int(text[idx+1:idy])
-
-    return x, y
-
-def getIneq(text):
-    text = text.replace(" ", "")
-    idx = text.find("x")
-    if idx < 0:
-        x = 0
-    else:
-        x = int(text[0:idx])
-
-    idy = text.find("y")
-    if idy < 0:
-        y = 0
-    else:
-        y = int(text[idx+1:idy])
-
-    idp = text.find("<")
-    case = "menor"
-    if idp < 0:
-        idp = text.find(">")
-        case = "mayor"
-    
-    ifr = text.find("=")
-    if ifr < 0:
-        r = text[idp+1:]
-    else:
-        r = text[ifr+1:]
-
-    return x, y, case, r
-
-
+def generateCurve(x, y, r):
+	xr = np.linspace(-50,50,50)
+	yr = np.linspace(-50,50,50)
+	if x == 0 and y != 0:
+		yr = np.repeat(r, 50)
+	if y == 0 and x != 0:
+		xr = np.repeat(r, 50)
+	else:
+		yr = (r-x*xr)/y
+	return xr, yr
 
 if __name__ == "__main__":
-    text = "-3x - 4y = 23"
-    print(getXAndY(text))
+	ejemplos = ["2x + 3y <= 9",
+		"-2x - 4y > 4",
+		"x + y < 1",
+		"x > -9",
+		"y <= 0",
+		"x > y",
+		"y <= x",
+		"y > 3x",
+		"-3y <= -2x",
+		"-7x > -10y",
+		"-y > -x",
+		"-x > -y",
+		"-x <= 3",
+		"y - x < 4"
+	]
 
-    text = "-6x + 9y >= 9"
-    print(getIneq(text))
-
-    text = "10x > -9"
-    print(getIneq(text))
-
-    text = "10y <= 1"
-    print(getIneq(text))
+	for i in ejemplos:
+		print("---------\n",i,": \n",stripIneq(i),sep="")
--- main.py
+++ main.py
@@ -10,139 +10,168 @@ import linear_programing as lp
 import extract as ext
 
 # ----------- GLOBALES ------------------------------------------------------------------------------
-#graph, toolbar
+# global graph, toolbar # Definidos en generarGrafico
 primeraVez = True
-listaInecuaciones = []
+inecuacionesLista = []
+curvas = []
+
+# Textos Default
+textoEjemploInecuacion = "Ejemplo: 2x-3y<=2"
+textoEjemploFuncionObjetivo = "Ejemplo: ax + by"
+
 
 # ----------- FUNCIONES ------------------------------------------------------------------------------
 def startLP():
-    # Desactivar la escritura del Textbox funcion objetivo
-    textboxFunc.configure(state="disabled")
+	entrada = textboxFunc.get()
+	if entrada != "" and entrada != textoEjemploFuncionObjetivo:
+		# Desactivar la escritura del Textbox funcion objetivo
+		textboxFunc.configure(state="disabled")
 
-    # Ejecutar funcion
-    pass
+		# Activa la escritura de Inecuaciones
+		textboxAdd.config(state="normal")
+		buttonAdd.config(state="normal")
+
+		# Ejecutar funcion
 
 
-def generarGrafico(xAxis, yAxis):
-    global graph, toolbar
+def generarGrafico():
+	global graph, toolbar, primeraVez, curvas
 
-    # Graph
-    figure = Figure(figsize=(4,3),dpi=100) 
-    subplot = figure.add_subplot(111) 
-    subplot.plot(xAxis,yAxis) # primera curva 
-    subplot.plot(yAxis,xAxis) # segunda curva 
-    graph = FigureCanvasTkAgg(figure, master=window)
-    graph.get_tk_widget().grid(row=0, column=1)
-    
-    # Toolbar
-    toolbar = tk.Frame(master=window)
-    toolbar.grid(row=1,column=1)
-    NavigationToolbar2Tk(graph, toolbar)
+	# Reiniciar la gráfica
+	if not primeraVez:
+		graph.get_tk_widget().grid_remove()
+		toolbar.grid_remove()
+	primeraVez = False
+	
+	# Iniciar Figure
+	figure = Figure(figsize=(5,4),dpi=100) 
+	subplot = figure.add_subplot(111)
 
+	# Graficar cada curva
+	for i in range(len(inecuacionesLista)):
+		subplot.plot(curvas[i][0],curvas[i][1],label=inecuacionesLista[i])
+
+	# Configuracion del subplot
+	subplot.legend()
+	subplot.grid()
+	subplot.set_xlim(-5,50)
+	subplot.set_ylim(-5,50)
+	subplot.spines['left'].set_position('zero')
+	subplot.spines['right'].set_color('none')
+	subplot.spines['bottom'].set_position('zero')
+	subplot.spines['top'].set_color('none')
+
+	# Gráfico
+	graph = FigureCanvasTkAgg(figure, master=window)
+	graph.get_tk_widget().grid(row=0, column=1)
+	
+	# Mostrar toolbar
+	toolbar = tk.Frame(master=window)
+	toolbar.grid(row=1,column=1)
+	NavigationToolbar2Tk(graph, toolbar)
 
 
 def addInecuation():
-    global graph, toolbar, primeraVez, listaInecuaciones
+	global graph, toolbar, primeraVez, inecuacionesLista
 
-    entrada = textboxAdd.get()+"\n"
-    if entrada != "\n":
-        # Agregar a la lista
-        listaInecuaciones.append(entrada)
-        
-        # Agregar al cuadro
-        cuadroIneq.config(state="normal")
-        cuadroIneq.insert("insert",entrada)
-        cuadroIneq.config(state="disable")
-        textboxAdd.delete(0,"end")
+	entrada = textboxAdd.get()
+	if entrada != "" and entrada != textoEjemploInecuacion:
+		# Quitar los espacios
+		entrada = entrada.replace(" ", "")
 
-        # Resetear Grafico
-        if not primeraVez:
-            graph.get_tk_widget().grid_remove()
-            toolbar.grid_remove()
+		# Agregar al cuadro
+		cuadroIneq.config(state="normal")
+		cuadroIneq.insert("insert",entrada+"\n")
+		cuadroIneq.config(state="disable")
+		textboxAdd.delete(0,"end")
 
-        # Generar Grafico
-        primeraVez = False
-        xAxis = list(range(100,0,-10))
-        yAxis = list(range(10))
-        generarGrafico(xAxis,yAxis)
+		# Agregar a la lista
+		inecuacionesLista.append(entrada)
 
-        # MAS MAS MAS
-    pass
+		# Agregar a la lista de curvas
+		xi, yi, ri = ext.stripIneq(entrada)
+		curvai = ext.generateCurve(xi,yi,ri)
+		curvas.append(curvai)
+
+		# Generar Grafico
+		generarGrafico()
+		# MAS MAS MAS
+
 
 def reset():
-    global graph, toolbar, primeraVez, listaInecuaciones
-    
-    # Limpiar textbox de funcion objetivo
-    textboxFunc.configure(state="normal")
-    textboxFunc.delete(0,"end")
-    textboxFunc.insert(0, "Ejemplo: ax + by")
-
-    # Limpiar textbox de inecuaciones
-    textboxAdd.delete(0,"end")
-    textboxAdd.insert(0,"Ejemplo: 2x+3y<=4")
-
-    # Limpiar cuadro list de inecuaciones
-    cuadroIneq.configure(state="normal")
-    cuadroIneq.delete("1.0",tk.END)
-    cuadroIneq.configure(state="disable")
-
-    # Limpiar la lista de inecuaciones
-    listaInecuaciones.clear()
-
-    # Remover los graficos y la barra de herramientas
-    if not primeraVez:
-            graph.get_tk_widget().grid_remove()
-            toolbar.grid_remove()
-
-    # Reiniciar la primera vez
-    primeraVez = True
+	global graph, toolbar, primeraVez, inecuacionesLista
+		
+	# Limpiar textbox de funcion objetivo
+	textboxFunc.configure(state="normal")
+	textboxFunc.delete(0,"end")
+	textboxFunc.insert(0, textoEjemploFuncionObjetivo)   
+	# Limpiar textbox de inecuaciones
+	textboxAdd.delete(0,"end")
+	textboxAdd.insert(0,textoEjemploInecuacion)
+	textboxAdd.config(state="disable")  
+	# Limpiar cuadro list de inecuaciones
+	cuadroIneq.configure(state="normal")
+	cuadroIneq.delete("1.0",tk.END)
+	cuadroIneq.configure(state="disable")   
+	# Desactivar el boton de Añadir
+	buttonAdd.config(state="disable")   
+	# Limpiar la lista de inecuaciones
+	inecuacionesLista.clear()   
+	# Remover los graficos y la barra de herramientas
+	if not primeraVez:
+		graph.get_tk_widget().grid_remove()
+		toolbar.grid_remove()   
+	# Reiniciar la primera vez
+	primeraVez = True
 
 
-# ----------- PROGRAMA ------------------------------------------------------------------------------
+# ----------- INTERFAZ ------------------------------------------------------------------------------
 # window
 window = tk.Tk()
 window.title("Programacion Lineal")
 window.config(bg="SystemButtonFace")
 
 #Canvas
-canvas = tk.Canvas(window, width=250, height=500)
+canvas = tk.Canvas(window, width=300, height=550)
 canvas.grid() # .pack()
 
 # Etiqueta Funcion Objetivo
 labelFunc = tk.Label(window, text="Ingrese funcion objetivo:")
-labelFunc.config(font=("Consolas",13))
-canvas.create_window(125, 10, window=labelFunc)
+labelFunc.config(font=("Consolas",15))
+canvas.create_window(150, 10, window=labelFunc)
 
 # Textbox Funcion Objetivo
-textboxFunc = ttk.Entry(window, justify=tk.CENTER, font=("Consolas",13))
-textboxFunc.insert(0, "Ejemplo: ax + by")
-canvas.create_window(125, 35, window=textboxFunc)
+textboxFunc = ttk.Entry(window, justify="center", font=("Consolas",15))
+textboxFunc.insert(0, textoEjemploFuncionObjetivo)
+canvas.create_window(150, 40, window=textboxFunc)
 
 # Boton Estabecer Funcion Objetivo
-buttonSet = tk.Button(window, text="Establecer",command=startLP, bg='palegreen', font=("Consolas",13)) 
-canvas.create_window(125, 70, window=buttonSet)
+buttonSet = tk.Button(window, text="Establecer",command=startLP, bg='palegreen', font=("Consolas",15)) 
+canvas.create_window(150, 80, window=buttonSet)
 
 # Etiqueta Inecuaciones
 labelAdd = tk.Label(window, text="Ingrese inecuaciones:")
-labelAdd.config(font=("Consolas",13))
-canvas.create_window(125, 105, window=labelAdd)
+labelAdd.config(font=("Consolas",15))
+canvas.create_window(150, 125, window=labelAdd)
 
 # Textbox Añadir Inecuacion
-textboxAdd = ttk.Entry(window, justify=tk.CENTER, font=("Consolas",13))
-textboxAdd.insert(0, "Ejemplo: 2x+3y<=4")
-canvas.create_window(125, 130, window=textboxAdd)
+textboxAdd = ttk.Entry(window, justify="center", font=("Consolas",15))
+textboxAdd.insert(0, textoEjemploInecuacion)
+textboxAdd.config(state="disabled")
+canvas.create_window(150, 155, window=textboxAdd)
 
 # Boton Añadir Inecuacion
-buttonAdd = tk.Button(window, text="Añadir",command=addInecuation, bg='palegreen', font=("Consolas",13)) 
-canvas.create_window(125, 165, window=buttonAdd)
+buttonAdd = tk.Button(window, text="Añadir",command=addInecuation, bg='palegreen', font=("Consolas",15))
+buttonAdd.config(state="disable")
+canvas.create_window(150, 195, window=buttonAdd)
 
 # Text Widget Lista de Inecuaciones
-cuadroIneq = tk.Text(window, width=19, height=11, padx=5, pady=5,bg='SystemButtonFace', font=("Consolas",13),state="disable")
-canvas.create_window(125, 320, window=cuadroIneq)
+cuadroIneq = tk.Text(window, width=19, height=11, padx=5, pady=5,bg='SystemButtonFace', font=("Consolas",15),state="disable")
+canvas.create_window(150, 360, window=cuadroIneq)
 
 # Boton Reiniciar
-buttonAdd = tk.Button(window, text="Reiniciar",command=reset, bg='pink', font=("Consolas",13)) 
-canvas.create_window(125, 475, window=buttonAdd)
+buttonReset = tk.Button(window, text="Reiniciar",command=reset, bg='pink', font=("Consolas",15)) 
+canvas.create_window(150, 525, window=buttonReset)
 
 # Loop Principal
 window.mainloop()
